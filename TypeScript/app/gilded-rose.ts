10e72091982b773bb99f115c7ecd21397a281a8d
Zak Smith <zsmith3.dev@gmail.com>
2021-07-06 15:10:00 +0100

    Refactor

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -11,59 +11,42 @@ export class Item {
 }
 
 export class GildedRose {
-    items: Array<Item>;
+    items: Item[];
 
-    constructor(items = [] as Array<Item>) {
+    constructor(items: Item[] = []) {
         this.items = items;
     }
 
     updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+        this.items = this.items.map(item => {
+            switch (item.name) {
+                case "Aged Brie":
+                    item.quality++;
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if (item.sellIn <= 0) item.quality = 0;
+                    else if (item.sellIn <= 5) item.quality += 3;
+                    else if (item.sellIn <= 10) item.quality += 2;
+                    else item.quality++;
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+                case "Conjured Mana Cake":
+                    item.quality -= 2;
+                    break;
+                default:
+                    if (item.sellIn <= 0) item.quality -= 2;
+                    else item.quality--;
+                    break;
             }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
-        }
 
+            item.quality = Math.max(item.quality, 0);
+            item.quality = Math.min(item.quality, 50);
+
+            if (item.name !== "Sulfuras, Hand of Ragnaros") item.sellIn--;
+
+            return item;
+        });
         return this.items;
     }
 }
